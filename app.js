46ffa71d9fcc780d227bdd3bf7818cb3a5488cb6
owais3 <khan6awais@gmail.com>
2023-09-03 20:20:51 +0500

    second commit

---
+++ app.js
@@ -0,0 +1,24 @@
+let userName = "Owais Nawab" 
+let myName = "Owais Nawab"
+let message = "Hello World"
+
+let age = "18 years old"
+let certificate = "Certified Mobile Application Development"
+
+let pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP"
+
+let email = "My Email address is khan6awais@gmail.com"
+let book = "I am trying to learn from the Book A smarter way to learn JavaScript"
+let heading ="Yah! I can write HTML content through JavaScript" 
+
+let storeEnd = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+alert(message)
+alert(myName)
+alert(age)
+alert(certificate)
+alert(pizza)
+alert(email)
+alert(book)
+alert(storeEnd)
+
+console.log(heading)
